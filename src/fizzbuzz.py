81028b0b2e4ae76c16f577e09e8f1eb99c125196
Ismail El Fekak <ismail.elfekak@gmail.com>
2022-03-10 16:59:53 +0100

    test #4
    When given a number that isn't a multiple of 3,5 or 15 return the number

--- src/fizzbuzz.py
+++ src/fizzbuzz.py
@@ -1,3 +1,8 @@
 def fizz_buzz_converter(a: int) -> str:
-    if a % 3 == 0:
+    if a % 3 == 0 and a % 5 != 0:
         return "fizz"
+    elif a % 5 == 0 and a % 3 != 0:
+        return "buzz"
+    elif a % 15 == 0:
+        return "fizzbuzz"
+    return str(a)
