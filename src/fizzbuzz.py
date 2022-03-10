a51f290078b6af0bd9c44ff43ea861bac78b54e5
Ismail El Fekak <ismail.elfekak@gmail.com>
2022-03-10 16:43:04 +0100

    test #3 : when given a multiple of 15 return "fizzbuzz"

--- src/fizzbuzz.py
+++ src/fizzbuzz.py
@@ -1,3 +1,7 @@
 def fizz_buzz_converter(a: int) -> str:
-    if a % 3 == 0:
+    if a % 3 == 0 and a % 5 != 0:
         return "fizz"
+    elif a % 5 == 0 and a % 3 != 0:
+        return "buzz"
+    elif a % 15 == 0:
+        return "fizzbuzz"
