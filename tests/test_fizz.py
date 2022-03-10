7d3a6ce4e1c26eb73ad0feb7e877cbb1b5c0dcdb
Ismail El Fekak <ismail.elfekak@gmail.com>
2022-03-10 16:36:31 +0100

    Update test_fizz.py

--- tests/test_fizz.py
+++ tests/test_fizz.py
@@ -4,3 +4,10 @@ from src.fizzbuzz import fizz_buzz_converter
 def test_when_number_is_multiple_of_3_should_return_fizz():
     assert fizz_buzz_converter(3) == "fizz"
 
+
+def test_when_number_is_multiple_of_5_should_return_buzz():
+    assert fizz_buzz_converter(5) == "buzz"
+
+
+def test_when_number_is_multiple_of_15_should_return_fizzbuzz():
+    assert fizz_buzz_converter(15) == "fizzbuzz"
